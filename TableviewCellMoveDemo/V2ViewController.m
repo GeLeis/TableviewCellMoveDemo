58a3b9612590870f8397b4b383963eedcb1801a9
zhaopan <869313996@qq.com>
2016-10-24 09:12:41 +0800

    add V2vc

---
+++ TableviewCellMoveDemo/V2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  V2ViewController.m
+//  TableviewCellMoveDemo
+//
+//  Created by zhaoP on 16/10/24.
+//  Copyright © 2016年 langya. All rights reserved.
+//
+
+#import "V2ViewController.h"
+
+@interface V2ViewController ()
+
+@end
+
+@implementation V2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
