6ab810337727e8cb2448fadc72f8511a3f4f3b25
zhaopan <869313996@qq.com>
2016-10-24 09:17:01 +0800

    add v4VC

---
+++ TableviewCellMoveDemo/V4fromMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  V4fromMasterViewController.h
+//  TableviewCellMoveDemo
+//
+//  Created by zhaoP on 16/10/24.
+//  Copyright © 2016年 langya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface V4fromMasterViewController : UIViewController
+
+@end
---
+++ TableviewCellMoveDemo/V4fromMasterViewController.m
@@ -0,0 +1,37 @@
+//
+//  V4fromMasterViewController.m
+//  TableviewCellMoveDemo
+//
+//  Created by zhaoP on 16/10/24.
+//  Copyright © 2016年 langya. All rights reserved.
+//
+
+#import "V4fromMasterViewController.h"
+
+@interface V4fromMasterViewController ()
+
+@end
+
+@implementation V4fromMasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
