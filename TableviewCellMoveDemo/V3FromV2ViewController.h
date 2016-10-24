299a9c8656f928a95a739a7a464411269aacd9d2
zhaopan <869313996@qq.com>
2016-10-24 09:15:04 +0800

    add v3 from v2

---
+++ TableviewCellMoveDemo/V3FromV2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  V3FromV2ViewController.h
+//  TableviewCellMoveDemo
+//
+//  Created by zhaoP on 16/10/24.
+//  Copyright © 2016年 langya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface V3FromV2ViewController : UIViewController
+
+@end
---
+++ TableviewCellMoveDemo/V3FromV2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  V3FromV2ViewController.m
+//  TableviewCellMoveDemo
+//
+//  Created by zhaoP on 16/10/24.
+//  Copyright © 2016年 langya. All rights reserved.
+//
+
+#import "V3FromV2ViewController.h"
+
+@interface V3FromV2ViewController ()
+
+@end
+
+@implementation V3FromV2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
